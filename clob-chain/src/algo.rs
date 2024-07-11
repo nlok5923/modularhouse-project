96ff5c9095796458d519e8f1a1ac606a5e21dc1b
nlok5923 <nlokhande5923@gmail.com>
2024-07-11 09:55:30 +0200

    Added module for algo runner

---
+++ clob-chain/src/algo.rs
@@ -0,0 +1,13 @@
+
+pub struct AlgoRunner;
+
+impl AlgoRunner {
+    pub fn new() -> AlgoRunner {
+        AlgoRunner
+    }
+
+    pub fn bfv_clob_algo() {
+        println!("Running bfv clob algo");
+    }
+}
+
